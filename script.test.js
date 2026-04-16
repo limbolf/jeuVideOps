d781085dac00b9c3471892f981ae9d99e2f45ec1
limbolf <vincentdecoopman0@gmail.com>
2026-04-16 09:59:13 +0200

    add second personnal test

--- script.test.js
+++ script.test.js
@@ -5,18 +5,13 @@
  */
 
 document.body.innerHTML = "<canvas></canvas>";
-// import {
-//   getRandomInt,
-//   rectIntersect,
-//   circleIntersect,
-//   timeToString,
-// } from "./js13k-spaceWord/script.js";
 
 const {
   getRandomInt,
   rectIntersect,
   circleIntersect,
   timeToString,
+  generateString,
 } = require("./js13k-spaceWord/script.js");
 
 test("PDF : getRandomInt inférieur à 43 retourne true", () => {
@@ -49,4 +44,28 @@ test("PDF : timeToString renvoie 17:36:78", () => {
 
 test("PDF : timeToString renvoie NAN", () => {
   expect(timeToString("toto")).toBe("NaN:NaN:NaN");
+});
+
+// ? Tests unitaires personnels
+
+test("Personnel 1 : Vérification de generateString", () => {
+  let alphabet = "abcdefghijklmnopqrstuvwxyz";
+  let longueurVoulue = 10;
+  let result = "";
+
+  for (let i = 0; i < longueurVoulue; i++) {
+    result += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+  }
+
+  expect(result.length).toBe(10);
+});
+
+test("Personnel 2 : Vérification des collisions", () => {
+  // ? On défini la position et le rayon des deux cercles
+  const c1 = { x: 0, y: 0, r: 10 };
+  const c2 = { x: 15, y: 0, r: 10 };
+
+  // ? La somme des rayons des cercles est 20 pixels, le deuxième cercle étant à seulement 15 pixels d'écart du premier cercle, les deux cercles se touchent (15 < 20).
+  const result = circleIntersect(c1.x, c1.y, c1.r, c2.x, c2.y, c2.r);
+  expect(result).toBe(true);
 });
